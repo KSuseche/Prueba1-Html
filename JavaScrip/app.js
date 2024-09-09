f5133c107ff82f70ea996b8af62a916d9a6ef01e
Karen Useche <sofiausecheramirez@gmail.com>
2024-09-09 11:53:43 -0500

    Aprendiendo JavaScritp

---
+++ JavaScrip/app.js
@@ -0,0 +1,20 @@
+console.log("Hello world!");
+
+const numeroPi = 3.1416;
+
+let numero1 = 10;
+let numero2 = 12;
+let suma = numero1 + numero2;
+let direccion = "calle 65g sur # 78b";
+
+
+
+console.log(numero1);
+console.log(numero2);
+console.log(suma);
+
+
+
+//comilla ivertida alt 96
+console.log(`Los numeros a sumar son: ${numero1}, ${numero2} y el total es: ${suma}`);
+console.log(`Mi dirección es: ${direccion}`);
